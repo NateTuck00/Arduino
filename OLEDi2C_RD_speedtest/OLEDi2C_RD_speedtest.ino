eb54a56ae93e2b143a81b358457ce22a86bcba8e
tuckern <nathantucker@holley.com>
2021-07-09 16:19:35 -0500

    Shows how concat can be used (and was used in the main project!) The timing of this project led to the choice to use it as the overall new display library

--- OLEDi2C_RD_speedtest/OLEDi2C_RD_speedtest.ino
+++ OLEDi2C_RD_speedtest/OLEDi2C_RD_speedtest.ino
@@ -1,16 +1,20 @@
 #include <OLED_I2C.h>
 
+#include <Wire.h>// out to save mem
+
 OLED myOLED(SDA, SCL);
 
 extern uint8_t SmallFont[];
 
+String test= "this work?";
+float flt=72.56;
 
 void setup() {
   // put your setup code here, to run once:
    if(!myOLED.begin(SSD1306_128X32))
     while(1);   // In case the library failed to allocate enough RAM for the display buffer...
 
-    
+   Wire.setClock(400000); 
    myOLED.setFont(SmallFont);
    pinMode(6,OUTPUT);
 }//end void setup()
@@ -21,7 +25,12 @@ void loop() {
   digitalWrite(6,HIGH);
 
   myOLED.clrScr();
-  myOLED.print("OLED_I2C text timeeee", LEFT, 0);
+ // test.concat(flt);
+ // test.concat(" Set:");
+  
+  myOLED.print(test, LEFT, 0);
+  
+ 
   myOLED.update();
 
   
