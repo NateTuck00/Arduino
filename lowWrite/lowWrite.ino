f3a02443f064d41f5e5eba4d9a384365fe0e8c66
tuckern <nathantucker@holley.com>
2021-08-09 10:06:35 -0500

    To write my actuators low in case of hardware issue

---
+++ lowWrite/lowWrite.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+  digitalWrite(1,LOW);
+  digitalWrite(2,LOW);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(1,LOW);
+  digitalWrite(2,LOW);
+}
