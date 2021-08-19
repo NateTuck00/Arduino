c60128b269841227f9403a4938c85b2496c488e8
tuckern <nathantucker@holley.com>
2021-08-19 14:09:23 -0500

    swapped pins

--- HBridgeTest/HBridgeTest.ino
+++ HBridgeTest/HBridgeTest.ino
@@ -1,17 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(1, OUTPUT);
+  pinMode(3, OUTPUT);
   pinMode(2,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(1,LOW);
+  digitalWrite(3,LOW);
   digitalWrite(2,LOW);
   delay(1000);
-  digitalWrite(1,HIGH);
+  digitalWrite(3,HIGH);
   delay(5000);
-  digitalWrite(1,LOW);
+  digitalWrite(3,LOW);
   delay(1000);
   digitalWrite(2,HIGH);
   delay(5000);
